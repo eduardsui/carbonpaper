56c243e8db6e8515d56ad4eaced061dc36a83efc
Eduard Suica <eduardsui@users.noreply.github.com>
2023-02-20 23:40:02 +0200

    Added ULL suffix

--- monocypher.c
+++ monocypher.c
@@ -463,10 +463,10 @@ void crypto_poly1305(u8     mac[16],  const u8 *message,
 /// BLAKE2 b ///
 ////////////////
 static const u64 iv[8] = {
-	0x6a09e667f3bcc908, 0xbb67ae8584caa73b,
-	0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1,
-	0x510e527fade682d1, 0x9b05688c2b3e6c1f,
-	0x1f83d9abfb41bd6b, 0x5be0cd19137e2179,
+	0x6a09e667f3bcc908ULL, 0xbb67ae8584caa73bULL,
+	0x3c6ef372fe94f82bULL, 0xa54ff53a5f1d36f1ULL,
+	0x510e527fade682d1ULL, 0x9b05688c2b3e6c1fULL,
+	0x1f83d9abfb41bd6bULL, 0x5be0cd19137e2179ULL,
 };
 
 static void blake2b_compress(crypto_blake2b_ctx *ctx, int is_last_block)
